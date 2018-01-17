bd5a8c057e0fb074bb427699bae5ac0e4b9bcf76
Max Ackerman <max@ackermax.com>
2018-01-16 20:41:27 -0500

    added gallery to photography page

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
+    $('.materialboxed').materialbox();
+    $('.carousel').carousel();
+    $(".carousel-item").click(function(){
+        var source = $(this).attr("src");
+        $("#image-dump").attr("src", source);
+    });
 });
